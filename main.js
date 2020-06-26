2a4652a935f10611254c09f3ac8ac6c3d6f6ced5
Jeri M <jerimiller@yahoo.com>
2020-06-26 16:15:15 -0400

    added images folder

---
+++ main.js
@@ -0,0 +1,26 @@
+let coin = {
+    state: 0,
+    flip: function() {
+       return this.state(Math.random())
+       
+       /* 1. Randomly set your coin object's "state" property to be either 
+       0 or 1: use "this.state" to access the "state" property on this object.*/
+    },
+    toString: function() {
+       if (state === 0) {
+           return 'Heads'
+       }else{
+           return 'Tails'
+       }
+        /* 2. Return the string "Heads" or "Tails", depending on whether
+        "this.state" is 0 or 1. */
+    },
+    toHTML: function() {
+        let image = document.createElement('img');
+        
+        /* 3. Set the properties of this image element to show either face-up
+        or face-down, depending on whether this.state is 0 or 1.*/
+        return image;
+    }
+};
+console.log(coin.state)
